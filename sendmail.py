41b9d396e09ba8c9592fa70f0dc82efa67ab761b
Jimtomr <jimtomr@hotmail.com>
2017-07-19 23:59:50 +0800

    first complete version

--- sendmail.py
+++ sendmail.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+
 
 class SendMail(object):
-    def __init__(self):
-        mail_host = 'smtp.bjceee.net.cn'
-        mail_user = 'user'
-        mail_pass = 'password'
+    def __init__(self, subject, value):
+        self.host = 'host'
+        self.from_addr = 'addr'
+        self.pw = 'password'
+        self.msg = self.makemsg(subject, value)
+
+    @staticmethod
+    def makemsg(subject, text):
+        msg = MIMEText(text, 'plain', 'utf-8')
+        msg['From'] = '123<123@sina.cn>'
+        msg['To'] = '456<456@163.com>'
+        msg['Subject'] = Header(u'预警:' + subject)
+        return msg
+
+    def send(self):
+        server = smtplib.SMTP(self.host, 25)
+        server.set_debuglevel(1)
+        server.login(self.from_addr, self.pw)
+        server.sendmail(self.from_addr, ['456@163.com'], self.msg.as_string())
+        server.quit()
+
+
+if __name__ == '__main__':
+    sender = SendMail(u'压力测试', u'2017-7-19 21:00:00 压力值 999')
+    sender.send()
