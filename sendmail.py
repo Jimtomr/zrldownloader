ab14591be8e2d6c37b7f442e705bd0f1e9f9a3d6
Panlei <jimtomr@163.com>
2017-07-19 17:56:03 +0800

    creat sendmail.py creat target_url.txt to save url

---
+++ sendmail.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+class SendMail(object):
+    def __init__(self):
+        mail_host = 'smtp.bjceee.net.cn'
+        mail_user = 'user'
+        mail_pass = 'password'
--- watchdog.py
+++ watchdog.py
@@ -7,7 +7,7 @@ from urllib2 import urlopen
 
 class WatchDog(object):
     def __init__(self):
-        self.target_url = 'http://47.94.1.242:10086/api/Snapshot?tagName=ZRL_ZRL.GX1%E6%8E%92%E7%84%A6%E7%9A%AE%E5%B8%A6%E6%8E%92%E7%84%A6%E6%B8%A9%E5%BA%A6%2CZRL_ZRL.GX1%E6%8E%92%E7%84%A6%E7%9A%AE%E5%B8%A6%E6%8E%92%E7%84%A6%E9%87%8D%E9%87%8F'
+        self.target_url = open('target_url.txt', 'r').read()
         self.value = {}
 
     def monitor(self):
@@ -23,8 +23,8 @@ class WatchDog(object):
                 print '---------%s changed---------' % data_label
                 self.value[data_label] = refresh_value
             print refresh_time, data_label, refresh_value
-        if float(self.value[u'排焦皮带排焦重量']) > 140.0:
-            pass
+        # if float(self.value[u'GX1排焦皮带排焦重量']) > 140.0:
+            # pass
 
     def refresh(self):
         self.monitor()
