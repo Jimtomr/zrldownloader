66038f258611e0ed3847e40dc91e82447191a47e
Panlei <jimtomr@163.com>
2017-07-20 17:45:33 +0800

    add error trapping

--- sendmail.py
+++ sendmail.py
@@ -6,28 +6,33 @@ from email.mime.text import MIMEText
 
 
 class SendMail(object):
-    def __init__(self, subject, value):
+    def __init__(self, subject, text):
         self.host = 'host'
         self.from_addr = 'addr'
-        self.pw = 'password'
-        self.msg = self.makemsg(subject, value)
+        self.pw = 'pass'
+        self.subject = subject
+        self.msg = self.makemsg(subject, text)
 
     @staticmethod
     def makemsg(subject, text):
         msg = MIMEText(text, 'plain', 'utf-8')
-        msg['From'] = '123<123@sina.cn>'
-        msg['To'] = '456<456@163.com>'
+        msg['From'] = 'Robot<@>'
+        msg['To'] = 'addr1<>, addr2<>'
         msg['Subject'] = Header(u'预警:' + subject)
         return msg
 
     def send(self):
-        server = smtplib.SMTP(self.host, 25)
-        server.set_debuglevel(1)
-        server.login(self.from_addr, self.pw)
-        server.sendmail(self.from_addr, ['456@163.com'], self.msg.as_string())
-        server.quit()
+        try:
+            server = smtplib.SMTP(self.host, 25)
+            # server.set_debuglevel(1)
+            server.login(self.from_addr, self.pw)
+            server.sendmail(self.from_addr, ['addr1', 'addr2'], self.msg.as_string())
+            server.quit()
+            print u'%s，已发送预警邮件' % self.subject
+        except Exception as e:
+            print u'邮件发送失败', e
 
 
 if __name__ == '__main__':
-    sender = SendMail(u'压力测试', u'2017-7-19 21:00:00 压力值 999')
+    sender = SendMail(u'功能测试', u'2017-7-1 00:00:00 测试数值 999')
     sender.send()
